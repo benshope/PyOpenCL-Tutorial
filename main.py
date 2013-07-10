eab50cd779ca92f899b54fd65d8447f0bf576565
Ben Shope <nimajnebs@gmail.com>
2013-07-10 12:13:18 -0700

    Add educational comments

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ kernel = """__kernel void multiply(__global float* a, __global float* b, __globa
 program = cl.Program(context, kernel).build() # compile the kernel into a Program
 # ??? Why doesn't compilation require the name of a specific device ???
 flags = cl.mem_flags
-# ??? What do these 'flag' things do ???
 
 a = numpy.array(range(1000), dtype=numpy.float32)
 b = numpy.array(range(1000), dtype=numpy.float32)  # Create two large float arrays
@@ -28,7 +27,12 @@ c_buffer = cl.Buffer(context, flags.WRITE_ONLY, b.nbytes)  # Allocate memory ???
 # ??? Am I allocating this memory on the host, device, or both ???
 
 program.multiply(queue, a.shape, None, a_buffer, b_buffer, c_buffer)
-# ??? Calling a routine on the compiled program seems a bit strange ???
+# call the program with arguments:
+# queue - the command queue this program will be sent to
+# a.shape - a tuple of the array's dimensions
+# None - ???
+# a.buffer, b.buffer, c.buffer - the three memory spaces this program deals with
+
 c = numpy.empty_like(a) # Create a correctly-sized array
 cl.enqueue_read_buffer(queue, c_buffer, c).wait()  # Execute everything and copy back c
 
