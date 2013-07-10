a4581ef1b66f201db01a831fe8a8b186db26eb03
Ben Shope <nimajnebs@gmail.com>
2013-07-10 02:49:17 -0700

    Add educational comments and todo commments

--- main.py
+++ main.py
@@ -15,19 +15,22 @@ program = cl.Program(context, kernel).build() # compile the kernel into a Progra
 # ??? Why doesn't compilation require the name of a specific device ???
 # It seems like this step would need a specific processor to compile to
 flags = cl.mem_flags
-# No idea what these 'flag' things do...
+# ??? What do these 'flag' things do ???
 
-a = numpy.array(range(100), dtype=numpy.float32)
-b = numpy.array(range(100), dtype=numpy.float32)  # Create two large random arrays
+a = numpy.array(range(1000), dtype=numpy.float32)
+b = numpy.array(range(1000), dtype=numpy.float32)  # Create two large float arrays
+# !!! It might be a better illustration if these were random !!!
 
 a_buf = cl.Buffer(context, flags.READ_ONLY | flags.COPY_HOST_PTR, hostbuf=a)
 b_buf = cl.Buffer(context, flags.READ_ONLY | flags.COPY_HOST_PTR, hostbuf=b)
-c_buf = cl.Buffer(context, flags.WRITE_ONLY, b.nbytes)
+c_buf = cl.Buffer(context, flags.WRITE_ONLY, b.nbytes)  # Allocate memory ???
+# ??? Am I allocating this memory on the host, device, or both ???
 
 program.multiply(queue, a.shape, None, a_buf, b_buf, c_buf)
-c = numpy.empty_like(a)
-cl.enqueue_read_buffer(queue, c_buf, c).wait()
+# ??? Calling a routine on the compiled program seems a bit strange ???
+c = numpy.empty_like(a) # Create a correctly-sized array
+cl.enqueue_read_buffer(queue, c_buf, c).wait()  # Execute everything and copy back c
 
 print "a", a
 print "b", b
-print "c", c
+print "c", c  # Print all the values
