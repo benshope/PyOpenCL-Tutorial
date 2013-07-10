259c8299b6b4d5fe76e59cf60aef2853f9819b9a
Ben Shope <nimajnebs@gmail.com>
2013-07-09 23:16:24 -0700

    Change fab file

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,9 @@
-"""
-    REQUIREMENTS:
-        - install pip with distribute (http://packages.python.org/distribute/)
-        - sudo pip install Fabric
-"""
-
 from fabric.api import local
 
 # Example:  fab run
 def run():
-    """ start the local app server """
-    local("python demo.py")
+    """ run the application """
+    local("python main.py")
 
 # Example:  fab commit:m="First commit"
 def update(m="Fab-update the app"):
