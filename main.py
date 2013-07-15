ce310dea777a2d0cc3d01d434d86fae626f32021
Ben Shope <nimajnebs@gmail.com>
2013-07-14 22:31:15 -0700

    Remove the import all commands

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # OpenCL + OpenGL program - visualization of particles with gravity
 
 import pyopencl as cl # OpenCL - GPU computing interface
-from OpenGL.GL import *  # OpenGL - GPU rendering interface
-from OpenGL.GLU import *  #  OpenGL tools (mipmaps, NURBS, perspective projection, shapes)
-from OpenGL.GLUT import *  # OpenGL tool to make a visualization window
+import OpenGL.GL as gl  # OpenGL - GPU rendering interface
+import OpenGL.GLU as glu  #  OpenGL tools (mipmaps, NURBS, perspective projection, shapes)
+import OpenGL.GLUT as glut  # OpenGL tool to make a visualization window
 import numpy # Tools to manipulate numbers
+import math # More tools to manipulate numbers
 import sys # System tools (path, modules, maxint)
 import time # What does this do?
 
@@ -12,7 +13,9 @@ context = cl.create_some_context()  # Create a Context (one per computer)
 queue = cl.CommandQueue(context)  # Create a Command Queue (one per processor)
 
 particles = 20000  # Number of particles
-time_step = .001  # Time between each frame
+time_step = .001  # Time between each animation frame
+window_width = 640
+window_height = 480
 
 kernel = """__kernel void part2(__global float4* pos, __global float4* color, __global float4* vel, __global float4* pos_gen, __global float4* vel_gen, float dt)
 {
@@ -54,6 +57,61 @@ kernel = """__kernel void part2(__global float4* pos, __global float4* color, __
     //here we adjust the alpha
     color[i].w = life;
 }"""  # The C-like code that will run on the GPU
+
+def initialize_window():
+    # Initialize the window
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+    glutInitWindowSize(window_width, window_height)
+    glutInitWindowPosition(0, 0)
+    self.win = glutCreateWindow("Part 2: Python")
+
+glutDisplayFunc(self.draw)  # Draw the current frame
+
+glutTimerFunc(30, self.timer, 30)  # Call draw every 30 milliseconds
+
+        # Set up OpenGL scene
+        self.glinit()
+        (pos_vbo, col_vbo, vel) = initialize.fountain(num)
+
+        # Create the OpenCL instance
+        self.cle = part2.Part2(num, dt)
+        self.cle.loadData(pos_vbo, col_vbo, vel)
+
+        glutMainLoop()
+
+    def glinit(self):
+        glViewport(0, 0, window_width, window_height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(60., window_width / float(window_height), .1, 1000.)
+        glMatrixMode(GL_MODELVIEW)
+
+    def draw():
+        """Render the particles"""
+        #update or particle positions by calling the OpenCL kernel
+        self.cle.execute(10)
+        glFlush()
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+        #handle mouse transformations
+        glTranslatef(self.initrans.x, self.initrans.y, self.initrans.z)
+        glRotatef(self.rotate.x, 1, 0, 0)
+        glRotatef(self.rotate.y, 0, 1, 0) #we switched around the axis so make this rotate_z
+        glTranslatef(self.translate.x, self.translate.y, self.translate.z)
+
+        #render the particles
+        self.cle.render()
+
+        #draw the x, y and z axis as lines
+        glutil.draw_axes()
+
+        glutSwapBuffers()
+
+
 
 program = cl.Program(context, kernel).build() # Compile the kernel into a Program
 
