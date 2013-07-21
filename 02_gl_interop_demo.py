b79a3a7150b03c7ac54f381893b5ea811ab512fb
Ben <nimajnebs@gmail.com>
2013-07-20 17:44:26 -0700

    Add helpful documentation

--- 02_gl_interop_demo.py
+++ 02_gl_interop_demo.py
@@ -4,62 +4,35 @@ from OpenGL.GL import *  # Import the GPU rendering interface
 from OpenGL.GLUT import *  # Import the OpenGL tool to make a visualization window
 from OpenGL.raw.GL.VERSION.GL_1_5 import glBufferData as rawGlBufferData  # Import C-style access to the buffers
 import pyopencl as cl  # Import the GPU computing interface
+from pyopencl.tools import get_gl_sharing_context_properties
+import sys
 
 num_vertices = 10000 # Why is this called vertices?
 
-kernel = """
-__kernel void generate_sin(__global float2* a)
+kernel = """__kernel void generate_sin(__global float2* a)
 {
-    int id = get_global_id(0);  /* Where are we in this array */
-    int global_size = get_global_size(0);  /* How big is this entire array */
-    float r = (float)id / (float)global_size;  /* r goes from 0 to 1 */
-    float x = r * 16.0f * 3.1415f;  /* Some math thing */
-    a[id].x = r * 2.0f - 1.0f; /* x coordinate of this graph point */
-    a[id].y = native_sin(x);  /* y coordinate of this graph point */
-}
-"""
+    int id = get_global_id(0);    /* Location in this array */
+    int global_size = get_global_size(0);    /* Size of this entire array */
+    float r = (float)id / (float)global_size;    /* r goes from 0 to 1 */
+    float x = r * 16.0f * 3.1415f;    /* Some math thing */
+    a[id].x = r * 2.0f - 1.0f;   /* X-coordinate of this graph point */
+    a[id].y = native_sin(x);    /* Y-coordinate of this graph point */
+}"""
 
 def initialize():
-    # Use the first vendor's OpenCL implementation installed on this machine
-    platform = cl.get_platforms()[0]
-
-    from pyopencl.tools import get_gl_sharing_context_properties
-    import sys
-    if sys.platform == "darwin":
-        context = cl.Context(properties=get_gl_sharing_context_properties(),
-                devices=[])
-    else:
-        # Some OSs prefer clCreateContextFromType, some prefer
-        # clCreateContext. Try both.
-        try:
-            context = cl.Context(properties=[
-                (cl.context_properties.PLATFORM, platform)]
-                + get_gl_sharing_context_properties())
-        except:
-            context = cl.Context(properties=[
-                (cl.context_properties.PLATFORM, platform)]
-                + get_gl_sharing_context_properties(),
-                devices = [platform.get_devices()[0]])
-
+    platform = cl.get_platforms()[0]  # Use the first vendor's OpenCL implementation installed on this machine
+    context = cl.Context(properties=[(cl.context_properties.PLATFORM, platform)] + get_gl_sharing_context_properties())  # Create a Context (one per computer)
+    """An OpenCL context is created with one or more devices. Contexts are used by the OpenCL runtime for managing objects such as command-queues, memory, program and kernel objects and for executing kernels on one or more devices specified in the context."""
     glClearColor(1, 1, 1, 1)
     glColor(0, 0, 1)
-    vertex_buffer = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer) # Link the vertex buffer to a target
-    """Different types of objects have different sets of targets in OpenGL.  
-    Each target has a specific meaning: to bind one object to one target means 
-    to use that object in whatever manner that target uses objects bound to it.
+    vertex_buffer = glGenBuffers(1)  # Generate the OpenGL Buffer
+    """Buffer Objects are OpenGL Objects that store an array of unformatted memory allocated by the OpenGL context (aka: the GPU). These can be used to store vertex data, pixel data retrieved from images or the framebuffer, and a variety of other things."""
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer) # Bind the vertex buffer to a target
+    """Different types of objects are bound to different sets of targets in GL.  Each target has a specific meaning: to bind one object to one target means to use that object in whatever manner that target uses objects bound to it.
 
-    The reason targets exist is because OpenGL is accessing special-purpose hardware
-    on the GPU.  Certain hardware (containing sets of functionality) is only 
-    available to certain memory areas and certain memory structures.
+    The GL_ARRAY_BUFFER target, for buffer objects, indicates the intent to use that buffer object for vertex data. However, binding to this target alone does not do anything; it's only the call to glVertexAttribPointer (or an equivalent function) that uses whatever buffer was bound to that target for the attribute data for that attribute.
 
-    To understand targets visually, think about a letter.  In order to send a letter you 
-    have to fill in several targets (State, Zip, City, Street...) in order to use 
-    the special-purpose hardware that is the postal system.  If you wrote in 
-    Alaska as the state, you couldn't also add Broadway as the street and 90210 
-    as the zip code.  The letter would arrive in Alaska, but the mechanism to get it to
-    Broadway would not be available from that point.
-    """
+    The reason targets exist is because OpenGL is accessing special-purpose hardware on the GPU.  Certain hardware (containing sets of functionality) is only available to certain memory structures."""
 
     rawGlBufferData(GL_ARRAY_BUFFER, num_vertices * 2 * 4, None, GL_STATIC_DRAW) # Allocate memory for the buffer
     glEnableClientState(GL_VERTEX_ARRAY)
@@ -85,7 +58,6 @@ def reshape(width, height):
     glMatrixMode(GL_MODELVIEW)
 
 if __name__ == '__main__':
-    import sys
     glutInit(sys.argv)
     if len(sys.argv) > 1:
         num_vertices = int(sys.argv[1])
--- README.md
+++ README.md
@@ -10,6 +10,10 @@ Comments can encourage bad style in production code, but they add a lot to a tut
 
 I have tried to write the code and the comments in a way that they could be separated and each one would still tell the complete story of what is going on.  With two sources of information, hopefully readers will seldom have to break focus and look something up.
 
+## Why are some lines super-long?  They go off my screen.
+
+It is better to turn on code-wrap in your editor than it is to bake in line breaks.
+
 ## Index
 
 - 01 Array Sum - A simple PyOpenCL program that sums two arrays
