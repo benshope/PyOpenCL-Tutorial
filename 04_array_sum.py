51a890e104e77cf9e07157cdc28e86fd9eb0d96b
Ben <nimajnebs@gmail.com>
2013-07-29 23:25:03 -0700

    Improve elementwise script

--- 04_array_sum.py
+++ 04_array_sum.py
@@ -1,3 +1,5 @@
+# Use OpenCL To Add Two Large Random Arrays (Using PyOpenCL Arrays and Elementwise)
+
 import pyopencl as cl
 import pyopencl.array as cl_array
 from pyopencl.elementwise import ElementwiseKernel
@@ -11,17 +13,14 @@ b = cl_array.to_device(queue, numpy.random.randn(10).astype(numpy.float32))
 c = cl_array.empty_like(a)
 
 linear_combination = ElementwiseKernel(context,
-        "float a, float *x, "
-        "float b, float *y, "
-        "float *z",
-        "z[i] = a*x[i] + b*y[i]",
+        "float *a, float *b, float *c",
+        "c[i] = a[i] + b[i]",
         "linear_combination")
-
-# arguments – a string formatted as a C argument list.
+# arguments – a string formatted as a C argument list
 # operation – a snippet of C that carries out the desired ‘map’ operation. The current index is available as the variable i. operation may contain the statement PYOPENCL_ELWISE_CONTINUE, which will terminate processing for the current element.
 # name – the function name as which the kernel is compiled
 
-linear_combination(5, a, 6, b, c)
+linear_combination(a, b, c)
 
 assert numpy.linalg.norm((c - (5*a+6*b)).get()) < 1e-5
 
