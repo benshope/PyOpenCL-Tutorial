7e6730022568b54a54d46d869e1aa5778ef4c1b4
Ben <nimajnebs@gmail.com>
2013-07-29 23:27:55 -0700

    Improve elementwise script

--- 04_array_sum.py
+++ 04_array_sum.py
@@ -13,11 +13,13 @@ b = cl_array.to_device(queue, numpy.random.randn(10).astype(numpy.float32))
 c = cl_array.empty_like(a)
 
 linear_combination = ElementwiseKernel(context,
-        "float *a, float *b, float *c",
+        "float *a, "
+        "float *b, "
+        "float *c",
         "c[i] = a[i] + b[i]",
         "linear_combination")
 # arguments – a string formatted as a C argument list
-# operation – a snippet of C that carries out the desired ‘map’ operation. The current index is available as the variable i. operation may contain the statement PYOPENCL_ELWISE_CONTINUE, which will terminate processing for the current element.
+# operation – a snippet of C that carries out the desired map operation
 # name – the function name as which the kernel is compiled
 
 linear_combination(a, b, c)
