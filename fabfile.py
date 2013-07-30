304faa8d04bd531fefcbbf452fece6e926125544
Ben <nimajnebs@gmail.com>
2013-07-30 11:16:15 -0700

    Change fabfile text

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import local
 
 # Example:  fab commit:m="First commit"
-def update(m="Fab-update the app"):
+def update(m="Auto-update the tutorial"):
     """ save the to github """
     local("git add .")
     local("git commit -a -m '{0}'".format(m))
