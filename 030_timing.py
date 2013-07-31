cab1d67a84ac47b93511bc118bae707084d6ef55
Ben <nimajnebs@gmail.com>
2013-07-30 23:39:21 -0700

    Refactor

--- 030_timing.py
+++ 030_timing.py
@@ -1,4 +1,4 @@
-# Use the Time module to test the speed of your program
+# Test the speed of your PyOpenCL program
 
 import pyopencl as cl
 import numpy
@@ -7,7 +7,7 @@ from time import time
 a = numpy.random.rand(1000).astype(numpy.float32)
 b = numpy.random.rand(1000).astype(numpy.float32)
 
-def cpu_array_sum():
+def cpu_array_sum(a, b):
     cpu_start_time = time()
     c_cpu = numpy.empty_like(a)
     for i in range(1000):
@@ -17,15 +17,13 @@ def cpu_array_sum():
     print("CPU Time: {0} s".format(cpu_end_time - cpu_start_time))
     return c_cpu
 
-def gpu_array_sum(): 
+def gpu_array_sum(a, b): 
     gpu_start_time = time()
     context = cl.create_some_context()
     queue = cl.CommandQueue(context, properties=cl.command_queue_properties.PROFILING_ENABLE)
-
     a_buffer = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=a)
     b_buffer = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=b)
     c_buffer = cl.Buffer(context, cl.mem_flags.WRITE_ONLY, b.nbytes)
-
     program = cl.Program(context, """
     __kernel void sum(__global const float *a, __global const float *b, __global float *c)
     {
@@ -40,14 +38,11 @@ def gpu_array_sum():
     event.wait()
     elapsed = 1e-9*(event.profile.end - event.profile.start)
     print("GPU Kernel Time: {0} s".format(elapsed))
-
     c_gpu = numpy.empty_like(a)
     cl.enqueue_read_buffer(queue, c_buffer, c_gpu).wait()
     gpu_end_time = time()
     print("GPU Time: {0} s".format(gpu_end_time - gpu_start_time))
     return c_gpu
 
-
-
-cpu_array_sum()
-gpu_array_sum()
+cpu_array_sum(a, b)
+gpu_array_sum(a, b)
