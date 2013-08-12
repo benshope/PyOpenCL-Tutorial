1749aec603582d3cbc1faed16344596759cca994
Ben <nimajnebs@gmail.com>
2013-08-12 03:14:56 -0700

    Refactor

--- 10_gl_particle_animation.py
+++ 10_gl_particle_animation.py
@@ -12,13 +12,17 @@ import time # XXX
 
 width = 800
 height = 600
+#number of particles
+num_particles = 50000
+#time step for integration
+time_step = .001
 
 class Part2(object):
-    def __init__(self, num, dt, *args, **kwargs):
-        clinit()
+    def __init__(self, num_particles, time_step, *args, **kwargs):
+        self.clinit()
         
         kernel = """
-        __kernel void part2(__global float4* pos, __global float4* color, __global float4* vel, __global float4* pos_gen, __global float4* vel_gen, float dt)
+        __kernel void part2(__global float4* pos, __global float4* color, __global float4* vel, __global float4* pos_gen, __global float4* vel_gen, float time_step)
         {
             //get our index in the array
             unsigned int i = get_global_id(0);
@@ -31,7 +35,7 @@ class Part2(object):
             //we've stored the life in the fourth component of our velocity array
             float life = vel[i].w;
             //decrease the life by the time step (this value could be adjusted to lengthen or shorten particle life
-            life -= dt;
+            life -= time_step;
             //if the life is 0 or less we reset the particle's values back to the original values and set life to 1
             if(life <= 0.f)
             {
@@ -42,11 +46,11 @@ class Part2(object):
 
             //we use a first order euler method to integrate the velocity and position (i'll expand on this in another tutorial)
             //update the velocity to be affected by "gravity" in the z direction
-            v.z -= 9.8f*dt;
+            v.z -= 9.8f*time_step;
             //update the position with the new velocity
-            p.x += v.x*dt;
-            p.y += v.y*dt;
-            p.z += v.z*dt;
+            p.x += v.x*time_step;
+            p.y += v.y*time_step;
+            p.z += v.z*time_step;
             //store the updated life in the velocity array
             v.w = life;
 
@@ -60,71 +64,68 @@ class Part2(object):
 
         }"""
 
-        program = cl.Program(ctx, kernel).build()
+        self.program = cl.Program(self.ctx, kernel).build()
 
-        num = num
-        dt = numpy.float32(dt)
+        self.num_particles = num_particles
+        self.time_step = numpy.float32(time_step)
 
 
     def loadData(self, pos_vbo, col_vbo, vel):
-        pos_vbo = pos_vbo
-        col_vbo = col_vbo
+        self.pos_vbo = pos_vbo
+        self.col_vbo = col_vbo
 
-        pos = pos_vbo.data
-        col = col_vbo.data
-        vel = vel
+        self.pos = pos_vbo.data
+        self.col = col_vbo.data
+        self.vel = vel
 
         #Setup vertex buffer objects and share them with OpenCL as GLBuffers
-        pos_vbo.bind()
+        self.pos_vbo.bind()
         #For some there is no single buffer but an array of buffers
         try:
-            pos_cl = cl.GLBuffer(ctx, mf.READ_WRITE, int(pos_vbo.buffer))
-            col_cl = cl.GLBuffer(ctx, mf.READ_WRITE, int(col_vbo.buffer))
+            self.pos_cl = cl.GLBuffer(self.ctx, mf.READ_WRITE, int(self.pos_vbo.buffer))
+            self.col_cl = cl.GLBuffer(self.ctx, mf.READ_WRITE, int(self.col_vbo.buffer))
         except AttributeError:
-            pos_cl = cl.GLBuffer(ctx, mf.READ_WRITE, int(pos_vbo.buffers[0]))
-            col_cl = cl.GLBuffer(ctx, mf.READ_WRITE, int(col_vbo.buffers[0]))
-        col_vbo.bind()
+            self.pos_cl = cl.GLBuffer(self.ctx, mf.READ_WRITE, int(self.pos_vbo.buffers[0]))
+            self.col_cl = cl.GLBuffer(self.ctx, mf.READ_WRITE, int(self.col_vbo.buffers[0]))
+        self.col_vbo.bind()
 
         #pure OpenCL arrays
-        vel_cl = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=vel)
-        pos_gen_cl = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=pos)
-        vel_gen_cl = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=vel)
-        queue.finish()
+        self.vel_cl = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=vel)
+        self.pos_gen_cl = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.pos)
+        self.vel_gen_cl = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.vel)
+        self.queue.finish()
 
         # set up the list of GL objects to share with opencl
-        gl_objects = [pos_cl, col_cl]
+        self.gl_objects = [self.pos_cl, self.col_cl]
         
     def execute(self, sub_intervals):
-        cl.enqueue_acquire_gl_objects(queue, gl_objects)
+        cl.enqueue_acquire_gl_objects(self.queue, self.gl_objects)
 
-        global_size = (num,)
+        global_size = (self.num_particles,)
         local_size = None
 
-        kernelargs = (pos_cl, 
-                      col_cl, 
-                      vel_cl, 
-                      pos_gen_cl, 
-                      vel_gen_cl, 
-                      dt)
+        kernelargs = (self.pos_cl, 
+                      self.col_cl, 
+                      self.vel_cl, 
+                      self.pos_gen_cl, 
+                      self.vel_gen_cl, 
+                      self.time_step)
 
         for i in xrange(0, sub_intervals):
-            program.part2(queue, global_size, local_size, *(kernelargs))
+            self.program.part2(self.queue, global_size, local_size, *(kernelargs))
 
-        cl.enqueue_release_gl_objects(queue, gl_objects)
-        queue.finish()
+        cl.enqueue_release_gl_objects(self.queue, self.gl_objects)
+        self.queue.finish()
  
     def clinit(self):
         platforms = cl.get_platforms()
         from pyopencl.tools import get_gl_sharing_context_properties
-        if sys.platform == "darwin":
-            ctx = cl.Context(properties=get_gl_sharing_context_properties(),
-                             devices=[])
-        else:
-            ctx = cl.Context(properties=[
-                (cl.context_properties.PLATFORM, platforms[0])]
-                + get_gl_sharing_context_properties(), devices=None)
+
+        self.ctx = cl.Context(properties=[
+            (cl.context_properties.PLATFORM, platforms[0])]
+            + get_gl_sharing_context_properties(), devices=None)
                 
-        queue = cl.CommandQueue(ctx)
+        self.queue = cl.CommandQueue(self.ctx)
 
 
     def render(self):
@@ -135,16 +136,16 @@ class Part2(object):
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
         #setup the VBOs
-        col_vbo.bind()
-        glColorPointer(4, GL_FLOAT, 0, col_vbo)
+        self.col_vbo.bind()
+        glColorPointer(4, GL_FLOAT, 0, self.col_vbo)
 
-        pos_vbo.bind()
-        glVertexPointer(4, GL_FLOAT, 0, pos_vbo)
+        self.pos_vbo.bind()
+        glVertexPointer(4, GL_FLOAT, 0, self.pos_vbo)
 
         glEnableClientState(GL_VERTEX_ARRAY)
         glEnableClientState(GL_COLOR_ARRAY)
         #draw the VBOs
-        glDrawArrays(GL_POINTS, 0, num)
+        glDrawArrays(GL_POINTS, 0, self.num_particles)
 
         glDisableClientState(GL_COLOR_ARRAY)
         glDisableClientState(GL_VERTEX_ARRAY)
@@ -152,17 +153,16 @@ class Part2(object):
         glDisable(GL_BLEND)
 
 
-def fountain_np(num):
+def fountain_np(num_particles):
     """numpy way of initializing data using ufuncs instead of loops"""
-    import numpy
-    pos = numpy.ndarray((num, 4), dtype=numpy.float32)
-    col = numpy.ndarray((num, 4), dtype=numpy.float32)
-    vel = numpy.ndarray((num, 4), dtype=numpy.float32)
+    pos = numpy.ndarray((num_particles, 4), dtype=numpy.float32)
+    col = numpy.ndarray((num_particles, 4), dtype=numpy.float32)
+    vel = numpy.ndarray((num_particles, 4), dtype=numpy.float32)
 
-    pos[:,0] = numpy.sin(numpy.arange(0., num) * 2.001 * numpy.pi / num) 
-    pos[:,0] *= numpy.random.random_sample((num,)) / 3. + .2
-    pos[:,1] = numpy.cos(numpy.arange(0., num) * 2.001 * numpy.pi / num) 
-    pos[:,1] *= numpy.random.random_sample((num,)) / 3. + .2
+    pos[:,0] = numpy.sin(numpy.arange(0., num_particles) * 2.001 * numpy.pi / num_particles) 
+    pos[:,0] *= numpy.random.random_sample((num_particles,)) / 3. + .2
+    pos[:,1] = numpy.cos(numpy.arange(0., num_particles) * 2.001 * numpy.pi / num_particles) 
+    pos[:,1] *= numpy.random.random_sample((num_particles,)) / 3. + .2
     pos[:,2] = 0.
     pos[:,3] = 1.
 
@@ -174,17 +174,17 @@ def fountain_np(num):
     vel[:,0] = pos[:,0] * 2.
     vel[:,1] = pos[:,1] * 2.
     vel[:,2] = 3.
-    vel[:,3] = numpy.random.random_sample((num, ))
+    vel[:,3] = numpy.random.random_sample((num_particles, ))
 
     return pos, col, vel
 
 
-def fountain(num):
+def fountain(num_particles):
     """Initialize position, color and velocity arrays we also make Vertex
     Buffer Objects for the position and color arrays"""
 
-    #pos, col, vel = fountain_loopy(num)
-    pos, col, vel = fountain_np(num)
+    #pos, col, vel = fountain_loopy(num_particles)
+    pos, col, vel = fountain_np(num_particles)
     
     #create the Vertex Buffer Objects
     from OpenGL.arrays import vbo 
@@ -196,109 +196,98 @@ def fountain(num):
     return (pos_vbo, col_vbo, vel)
     
 
-#number of particles
-num = 20000
-#time step for integration
-dt = .001
 
-class window(object):
-    def __init__(self, *args, **kwargs):
-        #mouse handling for transforming scene
+# GL Callbacks
+def timer(t):
+    glutTimerFunc(t, timer, t)
+    glutPostRedisplay()
+
+def on_key(*args):
+    print 'on key'
+    ESCAPE = '\033'
+    if args[0] == ESCAPE or args[0] == 'q':
+        sys.exit()
+
+def on_click(button, state, x, y):
+    print 'on click'
+    if state == GLUT_DOWN:
+        mouse_down = True
+        button = button
+    else:
         mouse_down = False
-        mouse_old = {'x': 0., 'y': 0.}
-        rotate = {'x': 0., 'y': 0., 'z': 0.}
-        translate = {'x': 0., 'y': 0., 'z': 0.}
-        initrans = {'x': 0., 'y': 0., 'z': -2.}
+    mouse_old['x'] = x
+    mouse_old['y'] = y
 
-        glutInit(sys.argv)
-        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
-        glutInitWindowSize(width, height)
-        glutInitWindowPosition(0, 0)
-        win = glutCreateWindow("Part 2: Python")
+def on_mouse_motion(x, y):
+    print 'on mouse motion'
+    dx = x - mouse_old['x']
+    dy = y - mouse_old['y']
+    if mouse_down and button == 0: #left button
+        rotate['x'] += dy * .2
+        rotate['y'] += dx * .2
+    elif mouse_down and button == 2: #right button
+        translate['z'] -= dy * .01 
+    mouse_old['x'] = x
+    mouse_old['y'] = y
 
-        #gets called by GLUT every frame
-        glutDisplayFunc(draw)
+def draw():
+    """Render the particles"""        
+    #update or particle positions by calling the OpenCL kernel
+    cle.execute(10) 
+    glFlush()
 
-        #handle user input
-        glutKeyboardFunc(on_key)
-        glutMouseFunc(on_click)
-        glutMotionFunc(on_mouse_motion)
-        
-        #this will call draw every 30 ms
-        glutTimerFunc(30, timer, 30)
-
-        #setup OpenGL scene
-        glinit()
-
-        #set up initial conditions
-        (pos_vbo, col_vbo, vel) = fountain(num)
-        #create our OpenCL instance
-        cle = Part2(num, dt)
-        cle.loadData(pos_vbo, col_vbo, vel)
-
-        glutMainLoop()
-        
-    def glinit(self):
-        glViewport(0, 0, width, height)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        gluPerspective(60., width / float(height), .1, 1000.)
-        glMatrixMode(GL_MODELVIEW)
-
-    # GL Callbacks
-    def timer(self, t):
-        glutTimerFunc(t, timer, t)
-        glutPostRedisplay()
-
-    def on_key(self, *args):
-        ESCAPE = '\033'
-        if args[0] == ESCAPE or args[0] == 'q':
-            sys.exit()
-
-    def on_click(self, button, state, x, y):
-        if state == GLUT_DOWN:
-            mouse_down = True
-            button = button
-        else:
-            mouse_down = False
-        mouse_old['x'] = x
-        mouse_old['y'] = y
-
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    rotate['x'] += .5
+    rotate['y'] += -.5
+    #handle mouse transformations
+    glTranslatef(initrans['x'], initrans['y'], initrans['z'])
+    glRotatef(rotate['x'], 1, 0, 0)
+    glRotatef(rotate['y'], 0, 1, 0) #we switched around the axis so make this rotate_z
+    glTranslatef(translate['x'], translate['y'], translate['z'])
     
-    def on_mouse_motion(self, x, y):
-        dx = x - mouse_old['x']
-        dy = y - mouse_old['y']
-        if mouse_down and button == 0: #left button
-            rotate['x'] += dy * .2
-            rotate['y'] += dx * .2
-        elif mouse_down and button == 2: #right button
-            translate['z'] -= dy * .01 
-        mouse_old['x'] = x
-        mouse_old['y'] = y
+    #render the particles
+    cle.render()
+
+    glutSwapBuffers()
 
 
-    def draw(self):
-        """Render the particles"""        
-        #update or particle positions by calling the OpenCL kernel
-        cle.execute(10) 
-        glFlush()
+#mouse handling for transforming scene
+mouse_down = False
+mouse_old = {'x': 0., 'y': 0.}
+rotate = {'x': 0., 'y': 0., 'z': 0.}
+translate = {'x': 0., 'y': 0., 'z': 0.}
+initrans = {'x': 0., 'y': 0., 'z': -2.}
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
+glutInit(sys.argv)
+glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+glutInitWindowSize(width, height)
+glutInitWindowPosition(0, 0)
+win = glutCreateWindow("Part 2: Python")
 
-        #handle mouse transformations
-        glTranslatef(initrans['x'], initrans['y'], initrans['z'])
-        glRotatef(rotate['x'], 1, 0, 0)
-        glRotatef(rotate['y'], 0, 1, 0) #we switched around the axis so make this rotate_z
-        glTranslatef(translate['x'], translate['y'], translate['z'])
-        
-        #render the particles
-        cle.render()
+#gets called by GLUT every frame
+glutDisplayFunc(draw)
 
-        glutSwapBuffers()
+#handle user input
+glutKeyboardFunc(on_key)
+glutMouseFunc(on_click)
+glutMotionFunc(on_mouse_motion)
 
+#this will call draw every 30 ms
+glutTimerFunc(30, timer, 30)
 
+#setup OpenGL scene
+glViewport(0, 0, width, height)
+glMatrixMode(GL_PROJECTION)
+glLoadIdentity()
+gluPerspective(60., width / float(height), .1, 1000.)
+glMatrixMode(GL_MODELVIEW)
 
-if __name__ == "__main__":
-    p2 = window()
+#set up initial conditions
+(pos_vbo, col_vbo, vel) = fountain(num_particles)
+#create our OpenCL instance
+cle = Part2(num_particles, time_step)
+cle.loadData(pos_vbo, col_vbo, vel)
+
+glutMainLoop()
