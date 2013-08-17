6c6fcd9cc58ae3b9deeb4ec4f65b0061a3325216
Ben <nimajnebs@gmail.com>
2013-08-17 02:06:29 -0700

    Refactor

--- 091_cl_gl_interop.py
+++ 091_cl_gl_interop.py
@@ -1,16 +1,14 @@
-# Game of life
-
 import pyopencl as cl
 mf = cl.mem_flags
 from pyopencl.tools import get_gl_sharing_context_properties
 from OpenGL.GL import *
 from OpenGL.raw.GL.VERSION.GL_1_5 import glBufferData as rawGlBufferData
-from OpenGL.GLU import *
 from OpenGL.GLUT import *
-import math
 import numpy
 
-num_points = 10000 # Number of data structures (vectors/vertices) describing points in 2D space.
+width = 800
+height = 600
+num_points = width * height
 
 kernel = """__kernel void generate_sin(__global float2* a)
 {
@@ -27,26 +25,27 @@ def display():
     glDrawArrays(GL_LINE_STRIP, 0, num_points)
     glFlush()
 
-
 glutInit()
-glutInitWindowSize(800, 600)
+glutInitWindowSize(width, height)
 glutCreateWindow('OpenCL/OpenGL Interop')
 glutDisplayFunc(display)
+glClearColor(1, 1, 1, 1)  # Set the background color to white
+glColor(0, 0, 0)  # Set the foreground color to black
 
 platform = cl.get_platforms()[0]
 context = cl.Context(properties=[(cl.context_properties.PLATFORM, platform)] + get_gl_sharing_context_properties())
-glClearColor(1, 1, 1, 1)  # Set the background color to white
-glColor(0, 0, 0)  # Set the foreground color to black
+
 vertex_buffer = glGenBuffers(1)  # Generate the OpenGL Buffer
 glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer) # Bind the vertex buffer to a target
-rawGlBufferData(GL_ARRAY_BUFFER, num_points * 2 * 4, None, GL_STATIC_DRAW) # Allocate memory for the buffer
-glEnableClientState(GL_VERTEX_ARRAY)  # The vertex array is enabled for writing and used during rendering when glDrawArrays is called
+rawGlBufferData(GL_ARRAY_BUFFER, num_points * 2 * 4, None, GL_STATIC_DRAW) # XXX Allocate memory for the buffer
+glEnableClientState(GL_VERTEX_ARRAY)  # XXX The vertex array is enabled for writing and used during rendering when glDrawArrays is called
 glVertexPointer(2, GL_FLOAT, 0, None)  # Define an array of vertex data (size, type, stride, pointer)
-cl_buffer = cl.GLBuffer(context, cl.mem_flags.READ_WRITE, int(vertex_buffer))  #  An OpenCL buffer object from an OpenGL buffer object (context, flags, GL buffer)
-prog = cl.Program(context, kernel).build()
+cl_buffer = cl.GLBuffer(context, cl.mem_flags.READ_WRITE, int(vertex_buffer))
+
+program = cl.Program(context, kernel).build()
 queue = cl.CommandQueue(context)
 cl.enqueue_acquire_gl_objects(queue, [cl_buffer])
-prog.generate_sin(queue, (num_points,), None, cl_buffer)
+program.generate_sin(queue, (num_points,), None, cl_buffer)
 cl.enqueue_release_gl_objects(queue, [cl_buffer])
 queue.finish()
 glFlush()
