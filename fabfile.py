07e0c6182f316a79126b6842ec641b3fbfd74075
benshope <nimajnebs@gmail.com>
2014-08-18 11:09:22 -0400

    Delete Fabfile

--- fabfile.py
+++
@@ -1,8 +0,0 @@
-from fabric.api import local
-
-# Example:  fab commit:m="First commit"
-def update(m="Auto-update"):
-    """ save the to github """
-    local("git add .")
-    local("git commit -a -m '{0}'".format(m))
-    local("git push")
